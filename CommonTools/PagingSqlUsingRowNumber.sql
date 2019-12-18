8267e0ad3f353ff82a2ae7c8b8b9a3813b9447ce
stonelv <zhe.lv.82@gmail.com>
2019-12-18 17:22:30 +0800

    Update PagingSqlUsingRowNumber.sql
    udpate real table name to TableName

--- CommonTools/PagingSqlUsingRowNumber.sql
+++ CommonTools/PagingSqlUsingRowNumber.sql
@@ -1,5 +1,3 @@
-select * from T_DT_Orders
-select * from [dbo].[T_DT_Logs] where [DTOrderID]=5
 
 declare @pageSize int
 declare @pageIndex int
@@ -9,10 +7,10 @@ set @pageIndex = 2
 SELECT * FROM 
 (
     SELECT TOP (@pageIndex * @pageSize) ROW_NUMBER() OVER (ORDER BY ID) AS RowNum, * 
-    FROM T_DT_Orders WHERE 1=1
+    FROM TableName WHERE 1=1
 ) AS tempTable
 WHERE RowNum > (@pageIndex-1)*@pageSize
 ORDER BY RowNum;
 
-select count(*) as totalCount from T_DT_Orders
+select count(*) as totalCount from TableName
 
